549c60a2d9e77a0420b57e2b5a4ce090859b6fe0
Tanmay Naik <tanmay_naik@persistent.com>
2021-03-30 20:35:51 +0530

    Updated server.js

--- source/server.js
+++ source/server.js
@@ -1,32 +1,54 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require("express");
+const app = express();
+const port = 8080;
 var msg;
 var app_msg = process.env.APP_MSG;
-var name= process.env.USER_NAME;
-var user_password= process.env.USER_PASSWORD;
+var name = process.env.USER_NAME;
+var user_password = process.env.USER_PASSWORD;
 
-app.get('/', (req, res) => {
-    if (app_msg == undefined) {
-        msg = 'Hello, ' + req.hostname + '. Time is ' + Date() + '.\nYou have no message for me. v3.\n';
-    } else {
-        msg = 'Hello, ' + req.hostname + '. Time is ' + Date() + '.\nI will deliver your message: ' + app_msg + '\n';
-    }
-    if (name != undefined && user_password != undefined){
-        msg = msg + '\nYour user name is :- ' + name + ' and password is :- ' + user_password + '\n';
-    }
-    res.send(msg);
-})
+app.get("/", (req, res) => {
+  if (app_msg == undefined) {
+    msg =
+      "Hello, " +
+      req.hostname +
+      ". Time is " +
+      Date() +
+      ".\nYou have no message for me. v3.\n";
+  } else {
+    msg =
+      "Hello, " +
+      req.hostname +
+      ". Time is " +
+      Date() +
+      ".\nI will deliver your message: " +
+      app_msg +
+      "\n";
+  }
+  if (name != undefined && user_password != undefined) {
+    msg =
+      msg +
+      "\nYour user name is :- " +
+      name +
+      " and password is :- " +
+      user_password +
+      "\n";
+  }
+  res.send(msg);
+});
 
-app.listen(port, () => console.log(`Sample node app listening at port ${port}`))
+app.listen(port, () =>
+  console.log(`Sample node app listening at port ${port}`)
+);
 
 // Sample code for SonarQube
 let corsOptions = {
-  origin: '*' // Sensitive
+  origin: "*", // Sensitive
 };
 
 function foo() {
   console.log("Hello, Tanmay!");
+  console.log("Hello, Vishal!");
+  console.log("Hello, World!");
 }
 
 a = foo();
@@ -38,6 +60,7 @@ var z = y.sort(); // Noncompliant
 var m = 8;
 var n = "8";
 
-if (m === n) {  // Noncompliant; always false
+if (m === n) {
+  // Noncompliant; always false
   // ...
 }
